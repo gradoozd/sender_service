242859dfa7cea27c858cd4631cb2a40dda329b1e
Roman Seleznev <roman.seleznev@altcms.local>
2022-11-13 17:36:24 +0300

    Подключен модуль body-parser

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
+require('body-parser-xml')(bodyParser);
 
 app.set('port', 3000)
+
+app.use(bodyParser.urlencoded( { extended: true }))
+app.use(bodyParser.xml({
+    xmlParseOptions: {
+      explicitArray: false // Only put nodes in array if >1
+    },
+}));
 
 app.post('/', (req, res, next) => {
     console.log('Received a request!', req.body)
