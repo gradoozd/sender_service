9f10159db2de6c3decd251c6de4bf84dc46d3fe6
Roman Seleznev <34817446+gradoozd@users.noreply.github.com>
2022-12-17 16:05:07 +0300

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(bodyParser.xml({
 
 app.post('/decision', (req, res, next) => {
     console.log('Received a request!', req.body)
-    res.status(200).end()
+    res.status(200).json({theme: null, version: 1.0})
     // setTimeout({
     //     fetch()
     // })
