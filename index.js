e4803edcb7ff7c9d9aa60009286fe852f456efed
Roman Seleznev <34817446+gradoozd@users.noreply.github.com>
2022-12-24 17:16:15 +0300

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(bodyParser.xml({
 }));
 
 app.post('/decision', (req, res, next) => {
-    console.log('Received a request!', req.body.contractConfirmation.header)
+    console.log('Received a request!', req.body)
     res.status(200).json({theme: null, version: 1.0})
     // setTimeout({
     //     fetch()
